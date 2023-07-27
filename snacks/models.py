2da9d0286eaa948f9c7c84fa0443f4b4a2fee67e
muhammad <moalsad001@gmail.com>
2023-07-27 10:36:07 +0000

    all done

---
+++ snacks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+# Create your models here.
+class Snack(models.Model):
+    title = models.CharField(max_length=255, help_text='snack title',default="unknown")
+    description = models.TextField(max_length=255,help_text="Description of the snack",default="no description available")
+    purchaser = models.ForeignKey(get_user_model(), on_delete= models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('snack_detail',args=[self.id])
